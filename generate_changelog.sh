51374005c004b5abf2582bc10578e361db4be5ac
marilynhantzis <ezdramiller@gmail.com>
2024-12-23 22:03:07 +0700

    init

---
+++ generate_changelog.sh
@@ -0,0 +1,17 @@
+   #!/bin/bash
+   
+   TAG=$(git describe --tags --abbrev=0)
+   DATE=$(date +%Y-%m-%d)
+   VERSION="v1.0.0"  # Замените на номер версии.
+
+   # Создадим новую секцию changelog
+   echo "## [$VERSION] - $DATE" >> changelog.md
+
+   # Получаем коммиты после последнего тега
+   COMMITS=$(git log $TAG..HEAD --pretty=format:"%s [%h]")
+
+   # Добавляем коммиты в changelog
+   while IFS= read -r commit; do
+       echo "- $commit" >> changelog.md
+   done <<< "$COMMITS"
+   
